5cc1faa65131c5c47e9b102b1c105e87fc3b558c
Curtis - <curtis_102@hotmail.com>
2019-04-03 03:03:31 -0700

    adding template - pug

--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,20 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res) => {
     res.sendFile(path.join(rootDir, "views", "add-product.html"));
-})
+});
 
 router.post('/add-product', (req, res) => {
-    console.log(req.body)
+    products.push({
+        title: req.body.title
+    })
     res.redirect('/');
 });
 
-module.exports = router;
+module.exports = {
+    routes: router,
+    products
+}
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 
-const rootDir = require('../utils/path'); 
+const rootDir = require('../utils/path');
+
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+    const products = adminData.products;
+    res.render("shop", {prods: products, docTitle: "Shop"});
+    /*
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    */
 })
  
 module.exports = router;
