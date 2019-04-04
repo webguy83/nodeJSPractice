0984bea391dad5221e07a95692046483f1a1777c
Curtis - <curtis_102@hotmail.com>
2019-04-04 01:40:01 -0700

    removal of pub and handlebar templates in favour of ejs

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const animalRoutes = require('./routes/animal')
 
 const app = express();
-
-// app.engine('hbs', expressHbs({
-//     defaultLayout: "main-layout",
-//     extname: "hbs"
-// }));
 
 app.set("view engine", "ejs");
 app.set("views","views");
@@ -30,7 +25,6 @@ app.use('/animals', animalRoutes);
 /* delete at some point */
 app.use((req, res, next) => {
     res.render('error', {docTitle: "Error and can't be found!"})
-    // res.status(404).sendFile(path.join(__dirname, "views", "error404.html"));
 })
 
 app.listen(3000);
